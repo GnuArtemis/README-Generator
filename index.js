063f209b41bf79c30f11e67f1b52fc53b9131cce
Ann Guinee <annmguinee@gmail.com>
2020-09-30 14:53:39 -0700

    Psuedocode and initial file structure

---
+++ index.js
@@ -0,0 +1,33 @@
+//npm init
+//install my dependencies - inquirer
+
+// Require all dependencies needed: inquirer fs
+
+//Create an array of questions
+
+//Write a README in a markdwon file as a template
+
+//function that will generate my readme template
+
+//Use inquirer to prompt users with questions
+
+//use answers that come back from inquirer - pass those into generate README function
+
+//write file using template generated from readme function
+
+// array of questions for user
+const questions = [
+
+];
+
+// function to write README file
+function writeToFile(fileName, data) {
+}
+
+// function to initialize program
+function init() {
+
+}
+
+// function call to initialize program
+init();
